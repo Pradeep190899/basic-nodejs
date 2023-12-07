10561924ba974f5d564300c47e81228dff182b11
pradeep190899 <pradeep98singh98@gmail.com>
2023-12-07 14:18:36 +0530

    added button

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const http = require('http')
 const ser = http.createServer((req,res)=>{
-    console.log( req.url,req.method,req.headers)
+    const url = req.url ;
+if(url=== "/home") {
+    res.write('<html>')
+    res.write('<head><title>2nd page</title></head>')
+    res.write('<body><h1>i am good</h1></body>')
+    res.write('</html>') 
+    return res.end()
+}
+  //  console.log( req.url,req.method,req.headers)
     res.setHeader('Content-type','text/html')
     res.write('<html>')
     res.write('<head><title>first page</title></head>')
